ab777880a0b7732ec52858c8adf05847b61eb719
zed-chi <zed-chi@mail.ru>
2023-03-16 16:41:29 +0300

    lint

--- calibrate.py
+++ calibrate.py
@@ -1,6 +1,6 @@
 from terminaltables import AsciiTable
 
-cur_list = [4,8,12,16,20]
+cur_list = [4, 8, 12, 16, 20]
 low = float(input("Нижний предел: "))
 high = float(input("Верхний предел: "))
 if input("выход тока 4-20? [y-n]: ").lower() == "n":
@@ -10,28 +10,24 @@ step = span / 4
 pressures = []
 
 
-if (low<high):
+if low < high:
     count = low
     for i in range(5):
         pressures.append(count)
-        count += step    
+        count += step
 else:
     count = high
     for i in range(5):
         pressures.append(count)
         count -= step
 print(pressures)
-results = {str(x):{} for x in pressures}
-for i in range(5):    
+results = {str(x): {} for x in pressures}
+for i in range(5):
     pres = str(pressures[i])
     input_cur = float(input(f"Ток при давлении {pres}: "))
     table_cur = cur_list[i]
     dev = round((input_cur - table_cur) / 16 * 100, 3)
-    results[pres]["up"] = {
-        "table":table_cur,
-        "real":input_cur,
-        "dev":dev
-    }
+    results[pres]["up"] = {"table": table_cur, "real": input_cur, "dev": dev}
     print(f" - Отклонение {input_cur} от {table_cur} = {dev}")
 
 for i in [*range(5)][::-1]:
@@ -39,33 +35,31 @@ for i in [*range(5)][::-1]:
     input_cur = float(input(f"Ток при давлении {pres}: "))
     table_cur = cur_list[i]
     dev = round((input_cur - table_cur) / 16 * 100, 3)
-    results[pres]["down"] = {
-        "table":table_cur,
-        "real":input_cur,
-        "dev":dev
-    }
+    results[pres]["down"] = {"table": table_cur, "real": input_cur, "dev": dev}
     print(f" - Отклонение {input_cur} от {table_cur} = {dev}")
 
 for i in range(5):
     pres = str(pressures[i])
-    results[pres]["var"] =round(abs(
-        results[pres]["down"]["dev"] -results[pres]["up"]["dev"]
-    ), 3)
+    results[pres]["var"] = round(
+        abs(results[pres]["down"]["dev"] - results[pres]["up"]["dev"]), 3
+    )
 data = []
 headers = ["Давл", "мА↓", "▲мА↓", "▲", "мА↑", "▲мА↑", "▲", "Вар"]
 data.append(headers)
 for i in pressures:
     pres = str(i)
-    data.append([
-        pres,
-        results[pres]["down"]["table"],
-        results[pres]["down"]["real"],
-        results[pres]["down"]["dev"],
-        results[pres]["up"]["table"],
-        results[pres]["up"]["real"],
-        results[pres]["up"]["dev"],
-        results[pres]["var"],
-    ])
+    data.append(
+        [
+            pres,
+            results[pres]["down"]["table"],
+            results[pres]["down"]["real"],
+            results[pres]["down"]["dev"],
+            results[pres]["up"]["table"],
+            results[pres]["up"]["real"],
+            results[pres]["up"]["dev"],
+            results[pres]["var"],
+        ]
+    )
 
 table = AsciiTable(data, title="Измерения")
 
--- measure.py
+++ measure.py
@@ -1,11 +1,11 @@
 class Measure:
-    def __init__(self, low, high, low_to_high=True, cur_list=[4,20]):
+    def __init__(self, low, high, low_to_high=True, cur_list=[4, 20]):
         x, y = cur_list
-        if low_to_high:            
-            self.cur_list = [*range(x, y+1, 4)]
+        if low_to_high:
+            self.cur_list = [*range(x, y + 1, 4)]
         else:
-            self.cur_list = [*range(y, x-1, -4)]
-        
+            self.cur_list = [*range(y, x - 1, -4)]
+
         self.low = low
         self.high = high
         self.span = max(low, high) - min(low, high)
@@ -13,17 +13,15 @@ class Measure:
         self.pressures = []
         self.set_pressures()
         self.results = {}
-        
-    
+
     def set_pressures(self):
-        if (self.low<self.high):
+        if self.low < self.high:
             count = self.low
             for i in range(5):
                 self.pressures.append(count)
-                count += self.step    
+                count += self.step
         else:
             count = self.high
             for i in range(5):
                 self.pressures.append(count)
                 count -= self.step
-    
